a479317673c74223d5b342d872109b2e62d01a3e
jackiezzz24 <99145834+jackiezzz24@users.noreply.github.com>
2022-02-27 15:49:02 -0500

    Create Week6_1.py

---
+++ Using Python to Access Web Data/Week6_1.py
@@ -0,0 +1,28 @@
+# Extracting Data from JSON
+
+# In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/json2.py. The program will prompt for a URL, read the JSON data from that URL using urllib and then parse and extract the comment counts from the JSON data, compute the sum of the numbers in the file and enter the sum below:
+# We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+# Sample data: http://py4e-data.dr-chuck.net/comments_42.json (Sum=2553)
+# Actual data: http://py4e-data.dr-chuck.net/comments_1401030.json (Sum ends with 10)
+
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+count0=0
+sum0=0
+while True:
+    url = input('Enter location:')
+    if len(url) <1: break
+
+    print('Retrieving', url)
+    data = urllib.request.urlopen(url, context=ctx).read().decode()
+    print('Retrieved', len(data),'characters')
+
+    info = json.loads(data)
